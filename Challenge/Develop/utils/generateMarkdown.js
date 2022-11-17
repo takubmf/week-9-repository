5d553b3043c02215d55cf7ae805e97c5782562a9
takubmf <90535455+takubmf@users.noreply.github.com>
2022-11-16 21:39:39 -0800

    finished code needs README

---
+++ Challenge/Develop/utils/generateMarkdown.js
@@ -0,0 +1,81 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== 'none' && license !== 'GNU LGPL') {
+    return `![Project License Badge](https://img.shields.io/badge/license-${license}-brightgreen)`
+  } else if (license === 'GNU LGPL') {
+    return `![Project License Badge](https://img.shields.io/badge/license-GNU-brightgreen)`
+  } else {
+    return '';
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (data.license === 'MIT') {
+    return`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+
+} else if (data.license === 'Unlicense') {
+  return`[![Unlicense]] (https://unlicense.org)`
+
+}else if (data.license === 'ISC') {
+  return`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+`
+
+}else if (data.license === 'GNU LGPL') {
+  return`[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
+
+}else {
+  return'';
+}
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (data.license !== 'none') {
+    return `
+  ## License
+  
+  ${renderLicenseLink(data)}
+`
+  } else {
+    return `
+  ## License
+  There has not been any license set for this project. 
+    `
+}
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#license)
+  * [Contributing](#Contributing)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+  ***
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ${renderLicenseSection(data)}
+  ## Contributing
+  ${data.contribution}
+  ## Tests
+  ${data.test}
+  
+  ## Questions
+  If you have any questions about the project you can reach out to me via email or GitHub with the information below. 
+  >Email: ${data.email} 
+  >GitHub : [${data.user}](https://github.com/${data.user})
+`;
+}
+
+module.exports = generateMarkdown;
