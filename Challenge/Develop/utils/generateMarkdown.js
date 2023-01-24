c5f1e074d684646a42706b84dffd871f9ee36089
takubmf <90535455+takubmf@users.noreply.github.com>
2023-01-24 10:27:08 -0800

    final solution

--- Challenge/Develop/utils/generateMarkdown.js
+++ Challenge/Develop/utils/generateMarkdown.js
@@ -13,17 +13,17 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (data.license === 'MIT') {
+  if (license === 'MIT') {
     return`[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
 
-} else if (data.license === 'Unlicense') {
+} else if (license === 'Unlicense') {
   return`[![Unlicense]] (https://unlicense.org)`
 
-}else if (data.license === 'ISC') {
+}else if (license === 'ISC') {
   return`[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
 `
 
-}else if (data.license === 'GNU LGPL') {
+}else if (license === 'GNU LGPL') {
   return`[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`
 
 }else {
@@ -34,11 +34,11 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (data.license !== 'none') {
+  if (license !== 'none') {
     return `
   ## License
   
-  ${renderLicenseLink(data)}
+  ${renderLicenseLink(license)}
 `
   } else {
     return `
@@ -65,7 +65,7 @@ function generateMarkdown(data) {
   ${data.install}
   ## Usage
   ${data.usage}
-  ${renderLicenseSection(data)}
+  ${renderLicenseSection(data.license)}
   ## Contributing
   ${data.contribution}
   ## Tests
